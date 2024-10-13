1f5e3953fda0726605a7723546457fe27547a325
endot1231 <endot1231@gmail.com>
2024-10-13 21:42:04 +0900

    BlankStringRuleのテストケースを追加

--- spec/lib/rules/BlankStringRule.rb
+++ spec/lib/rules/BlankStringRule.rb
@@ -1,9 +1,25 @@
-class BlankStringRule  
-  def match?(carry,n)
-    carry == ""
+require './lib/rules/BlankStringRule'
+
+RSpec.describe 'BlankStringRule' do
+  let(:rule) { BlankStringRule.new }
+
+  describe '#match?' do
+    context '空文字の場合' do
+      it 'returns true' do
+        expect(rule.match?("", 5)).to be true
+      end
+    end
+
+    context '空文字以外の場合' do
+      it 'returns false' do
+        expect(rule.match?("Fizz", 5)).to be false
+      end
+    end
   end
 
-  def apply(carry,n)
-    n.to_s
-  end    
+  describe '#apply' do
+    it '入力された文字をそのまま返却' do
+      expect(rule.apply("", 5)).to eq('5')
+    end
+  end
 end
